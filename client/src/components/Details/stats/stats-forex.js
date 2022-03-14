f63f53698c4c604592c12e866277e5c1bbeb1590
Ricky Thain <rickythain@gmail.com>
2022-03-14 02:27:26 +0000

    create keys for list items

--- client/src/components/Details/stats/stats-forex.js
+++ client/src/components/Details/stats/stats-forex.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Stack, Paper } from "@mui/material";
+import { Grid } from "@mui/material";
 import "./stats-forex.css";
 
 function DataTablet({ name, value, strength = null }) {
@@ -45,6 +45,8 @@ const getBgColor = (strength) => {
     case "strong":
       colorClass = "tablet-strong";
       break;
+    default:
+      colorClass = "tablet-default";
   }
   return colorClass;
 };
@@ -60,12 +62,13 @@ export default function StatsForex({ statsData }) {
           <Grid container spacing={2}>
             <Grid item xs={6}>
               {statsData ? (
-                statsData.support.map((item) => {
+                statsData.support.map((item, index) => {
                   return (
                     <DataTablet
                       name={item.s}
                       value={item.value}
                       strength={item.strength}
+                      key={item.s + index.toString()}
                     />
                   );
                 })
@@ -75,12 +78,13 @@ export default function StatsForex({ statsData }) {
             </Grid>
             <Grid item xs={6}>
               {statsData ? (
-                statsData.resist.map((item) => {
+                statsData.resist.map((item, index) => {
                   return (
                     <DataTablet
                       name={item.r}
                       value={item.value}
                       strength={item.strength}
+                      key={item.r + index.toString()}
                     />
                   );
                 })
@@ -91,9 +95,9 @@ export default function StatsForex({ statsData }) {
           </Grid>
           <div className="strength-indicators-group">
             <Grid container justifyContent="center" spacing={2}>
-              {strengths.map((strength) => {
+              {strengths.map((strength, index) => {
                 return (
-                  <Grid item xs={4}>
+                  <Grid item xs={4} key={strength + index.toString()}>
                     <StrengthIndicatorTablet strength={strength} />
                   </Grid>
                 );
@@ -107,42 +111,50 @@ export default function StatsForex({ statsData }) {
           <div>
             <h3>Pivot Points</h3>
           </div>
-          <Grid container spacing={0}>
+          <Grid container spacing={0} key="grid-0">
             <Grid item xs={4}>
               {statsData ? (
-                statsData.pivot.map((item) => {
+                statsData.pivot.map((item, index) => {
                   if (item.pivot.toLowerCase() === "p") {
-                    return <DataTablet name={item.pivot} value={item.point} />;
-                  }
+                    return (
+                      <DataTablet
+                        name={item.pivot}
+                        value={item.point}
+                        key={item.pivot + index.toString()}
+                      />
+                    );
+                  } else return null;
                 })
               ) : (
                 <p>loading...</p>
               )}
             </Grid>
           </Grid>
-          <Grid container spacing={0}>
+          <Grid container spacing={0} key="grid-1">
             {statsData ? (
-              statsData.pivot.map((item) => {
-                if (item.pivot.toLowerCase().includes("s"))
+              statsData.pivot.map((item, index) => {
+                if (item.pivot.toLowerCase().includes("s")) {
                   return (
-                    <Grid item xs={4}>
+                    <Grid item xs={4} key={item.pivot + index.toString()}>
                       <DataTablet name={item.pivot} value={item.point} />
                     </Grid>
                   );
+                } else return null;
               })
             ) : (
               <p>loading...</p>
             )}
           </Grid>
-          <Grid container spacing={0}>
+          <Grid container spacing={0} key="grid-2">
             {statsData ? (
-              statsData.pivot.map((item) => {
-                if (item.pivot.toLowerCase().includes("r"))
+              statsData.pivot.map((item, index) => {
+                if (item.pivot.toLowerCase().includes("r")) {
                   return (
-                    <Grid item xs={4}>
+                    <Grid item xs={4} key={item.pivot + index.toString()}>
                       <DataTablet name={item.pivot} value={item.point} />
                     </Grid>
                   );
+                } else return null;
               })
             ) : (
               <p>loading...</p>
